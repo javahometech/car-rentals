b3da44f5c7bd87101ab88f6b7d337e331bce0a9d
javahometech <kammana.hari@gmail.com>
2020-04-29 08:07:54 +0530

    Create Calculator.java

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator{
+
+public void add(int i, int j){
+   int result = i+j;
+   String name = null;
+   name.toString()
+}
+
+private void hello(){
+   System.out.println("Welcome");
+}
+
+
+}
